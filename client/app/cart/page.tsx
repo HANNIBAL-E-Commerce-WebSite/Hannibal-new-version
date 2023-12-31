f29997773cd182f35d5dcf5702608873cab93d8e
salmenkhelifi1 <salmen@base.tn>
2024-01-01 00:26:07 +0100

    wish+lauding+cart

---
+++ client/app/cart/page.tsx
@@ -0,0 +1,155 @@
+"use client";
+
+import { Fragment, useState } from "react";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import Badge from "@mui/material/Badge";
+import Button from "@mui/material/Button";
+import axios from "axios";
+import { IconButton } from "@mui/material";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import RemoveCircleIcon from "@mui/icons-material/RemoveCircle";
+import HighlightOffIcon from "@mui/icons-material/HighlightOff";
+import "./Cart.css"
+// Import CartCard and any other components or contexts you might be using
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  minWidth: "350px",
+  width: "45%",
+  height: "400px",
+  bgcolor: "background.paper",
+  border: "5px solid #FFE26E",
+  borderRadius: "15px",
+  boxShadow: 24,
+  p: 4,
+};
+
+const Cart = () => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const [openCheckoutModal, setOpenCheckoutModal] = useState(false);
+  const handleCheckoutOpen = () => setOpenCheckoutModal(true);
+  const handleCheckoutClose = () => setOpenCheckoutModal(false);
+  const handelQuantityIncrement = (event) => {
+    cartItems.quantity(props.item.id, "INC");
+  };
+
+  const handelQuantityDecrement = (event) => {
+    if (props.item.itemQuantity > 1) {
+      cartItems.quantity(props.item.id, "DEC");
+    }
+  };
+
+  const handelRemoveItem = () => {
+    cartItems.removeItem(props.item);
+  };
+
+  const cartItems: any = {};
+  const handleCheckout = async () => {
+    if (cartItems.totalAmount > 0) {
+      const config = {
+        reason: "checkout",
+        amount: cartItems.totalAmount,
+      };
+
+      try {
+        const res = await axios.post("/api/payment", config);
+        console.log(res.data);
+        window.location.replace(res.data);
+        handleCheckoutOpen();
+      } catch (err) {
+        console.log(err);
+      }
+    } else {
+      return;
+    }
+  };
+  return (
+    <Fragment>
+      <Badge badgeContent="5" color="error">
+        <ShoppingCartIcon
+          color="black"
+          onClick={handleOpen}
+          sx={{ width: '35px' }}
+        />
+      </Badge>
+      <Modal open={open} onClose={handleClose}>
+        <Box sx={style}>
+          <div className="cart__header">
+            <h2>Your Cart</h2>
+          </div>
+          <div className="cart__items__container">
+            <div className="cartItems">
+              {5 === 0 ? (
+                <div className="cart__empty"> Empty cart!</div>
+              ) : (
+                <div className="shop__cart__items">
+                 <div className="cart__item__card">
+      <div className="cart__item__detail">
+        <div className="cart__item__image">
+          <img
+            src=""
+            alt="item"
+            className="item__image"
+          />
+        </div>
+        <div className="cart__item__name">test</div>
+      </div>
+      <div className="cart__item__quantity">
+        <IconButton onClick={handelQuantityIncrement}>
+          <AddCircleIcon />
+        </IconButton>
+        <div type="text" name="quantity" className="quantity__input">
+          "10"
+        </div>
+        <IconButton onClick={handelQuantityDecrement}>
+          <RemoveCircleIcon fontSize="medium" />
+        </IconButton>
+      </div>
+  
+      <div className="cart__item__price">$55</div>
+      <div className="remove__item__icon">
+        <IconButton>
+          <HighlightOffIcon onClick={handelRemoveItem} />
+        </IconButton>
+      </div>
+    </div>
+                </div>
+              )}
+              {3 > 0 && (
+                <div className="options">
+                  <div className="total__amount">
+                    <div className="total__amount__label">Total Amount:</div>
+                    <div className="total__amount__value">
+                      ${cartItems.totalAmount}.00
+                    </div>
+                  </div>
+                  <div className="checkout">
+                    <Button variant="outlined" onClick={handleCheckout}>
+                      Checkout
+                    </Button>
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
+        </Box>
+      </Modal>
+      <Modal open={openCheckoutModal} onClose={handleCheckoutClose}>
+        <Box sx={style}>
+          <div className="d-flex w-100 h-100 justify-content-center align-items-center">
+            <h2>Your checkout was successful</h2>
+          </div>
+        </Box>
+      </Modal>
+    </Fragment>
+  );}
+
+export default Cart;
